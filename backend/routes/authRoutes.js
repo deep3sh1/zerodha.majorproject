d1f5523711fcef6b0c508bd5489a315de9800c4a
deep3sh1 <deepeshw3108@gmail.com>
2025-12-08 19:05:54 +0530

    Update authRoutes.js
     c INTO C

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login } = require("../controllers/authController");
+const { signup, login } = require("../Controllers/authController");
 
 // Signup route
 router.post("/signup", signup);
@@ -9,3 +9,4 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 module.exports = router;
+

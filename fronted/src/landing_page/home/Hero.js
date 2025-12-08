940b80320d208b6569f789c9d9724c60b15f9a26
deep3sh1 <deepeshw3108@gmail.com>
2025-12-08 20:27:23 +0530

    Update Hero.js
    change the image hero

--- fronted/src/landing_page/home/Hero.js
+++ fronted/src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
     return ( 
     <div className='container p-5'>
         <div className='row text-center' >
-            <img src='media/images/homehero.png' alt='hero-img ' className='mb-5'/>
+            <img src='media/images/homehero.png' alt='homeHero.png' className='mb-5'/>
             <h1 className='mt-5'>Invest in everything</h1>
             <p >Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
         <button className='p-2 btn btn-primary fs-5 mb-5' style={{  width: "20%", margin: "0 auto" }}>Sign up now</button>
@@ -14,5 +14,6 @@ function Hero() {
     </div>
      );
 }
+
 
 export default Hero;
